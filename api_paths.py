c52013de097835e8e9e6bbbe6ba3b3305eafe6bc
BenjaminVegaG <159029839+BenjaminVegaG@users.noreply.github.com>
2025-07-03 14:18:07 -0400

    Benja_Changes
    Additional Benja.py code

--- api_paths.py
+++ api_paths.py
@@ -1,5 +1,6 @@
 api_paths = {
         "Simón": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP9\Python\3.10",
         "Oscar": r"",
-        "Kelly PF 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10"
+        "Kelly PF 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10",
+        "Benja PF 2024": r"C:\Program Files\DIgSILENT\PowerFactory 2024 SP2\Python\3.10"
     }
