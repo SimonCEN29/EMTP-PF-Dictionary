b49ec701e3b83cb741fabe598596659e45cdce03
SimonCEN29 <simon.veloso@coordinador.cl>
2025-07-04 12:00:48 -0400

    Adding Cristóbal

--- api_paths.py
+++ api_paths.py
@@ -1,6 +1,7 @@
 api_paths = {
         "Simón": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP9\Python\3.10",
-        "Oscar": r"",
+        "Cris 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10",
+        "Cris 2024": r"",
         "Kelly PF 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10",
         "Benja PF 2024": r"C:\Program Files\DIgSILENT\PowerFactory 2024 SP2\Python\3.10"
     }
---
+++ cristobal.py
@@ -0,0 +1,28 @@
+import os
+import user_tools
+import powerfactory_api
+import pf_tools
+from pathlib import Path
+
+if __name__ == '__main__':
+    os.system('cls')
+
+    
+    pf_user = powerfactory_api.PfHandler("Cris 2022")
+    app = pf_user.start_powerfactory()
+    pf_user.activate_project(r"\cristobal.barrera\2505-BD-OP-COORD-DMAP")
+
+    data = pf_tools.PfData(app)
+    data.run_load_flow()
+
+    csv_headers = {
+        "PowerSystemResource": ["PsrID", "FullName", "ShortName", "Extension"],
+        "Terminal": ["TermID", "PsrID", "TerminalSideID", "MW", "Mvar", "u_mag", "u_deg"],
+        "Load": ["LoadID", "PsrID"],
+        "Src": ["SrcID", "PsrID"]
+    }
+
+    dir = user_tools.Directory(r"C:\Git Codes\EMTP-FP Dic Output")
+    csv_files = dir.print_headers(csv_headers, "latin-1")
+
+    data.get_elms(csv_files, csv_headers, "latin-1")
