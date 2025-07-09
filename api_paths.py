58b395a95dc4fe2e632064f5283060c2c7d3e1df
SimonCEN29 <simon.veloso@coordinador.cl>
2025-07-09 13:29:07 -0400

    User Benja PF 2023 added

--- api_paths.py
+++ api_paths.py
@@ -3,5 +3,6 @@ api_paths = {
         "Cris 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10",
         "Cris 2024": r"",
         "Kelly PF 2022": r"C:\Program Files\DIgSILENT\PowerFactory 2022 SP3\Python\3.10",
-        "Benja PF 2024": r"C:\Program Files\DIgSILENT\PowerFactory 2024 SP2\Python\3.10"
+        "Benja PF 2024": r"C:\Program Files\DIgSILENT\PowerFactory 2024 SP2\Python\3.10",
+        "Benja PF 2023": r"C:\Program Files\DIgSILENT\PowerFactory 2023 SP3A\Python\3.10"
     }
